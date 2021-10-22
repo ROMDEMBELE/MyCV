99b716660d85ea6d3d285aa812dcc110cd0ad764
Romain Dembele <romain.dembele@diabeloop.fr>
2021-10-22 09:50:08 +0200

    fix UserRepositoryModule

--- data/src/main/java/com/diabeloop/sample/architecture/data/di/RepositoryModule.kt
+++ data/src/main/java/com/diabeloop/sample/architecture/data/di/RepositoryModule.kt
@@ -1,5 +1,6 @@
 package com.diabeloop.sample.architecture.data.di
 
+import com.diabeloop.sample.architecture.data.database.doa.UserDao
 import com.diabeloop.sample.architecture.data.repository.UserRepositoryImpl
 import com.diabeloop.sample.architecture.domain.user.UserRepository
 import dagger.Module
@@ -13,5 +14,5 @@ import javax.inject.Singleton
 class RepositoryModule {
     @Provides
     @Singleton
-    fun provideMoviesRepository(): UserRepository = UserRepositoryImpl()
+    fun provideMoviesRepository(userDao: UserDao): UserRepository = UserRepositoryImpl(userDao)
 }
